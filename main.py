8dbed1fcb2c107cf19ec5c47af89642397487515
SpyJK <JKspys@proton.me>
2025-11-21 15:39:51 +0000

    teste de subsCallback

--- main.py
+++ main.py
@@ -98,7 +98,8 @@ def callback_query(call):
         menu_xmr(chat_id)
     elif call.data == "usdt":
         menu_usdt(chat_id)
-
+    elif call.data == "moeda_XMR_5":
+        menu_usdt(chat_id)
 # ==========================
 #     EXECUTAR TUDO
 # ==========================
